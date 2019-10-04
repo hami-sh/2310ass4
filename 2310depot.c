14c2d4cff1f1ce42e028d214423084dbd1e9d60a
Hamish Bultitude <sendanemailtohamish@gmail.com>
2019-10-04 21:05:15 +1000

    arg checking

--- 2310depot.c
+++ 2310depot.c
@@ -1,5 +1,126 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+typedef enum {
+    OK = 0,
+    INCORRARGS = 1,
+    NAMEERR = 2,
+    QUANERR = 3
+} Status;
+
+// struct for items
+typedef struct {
+    char* name;
+    int count;
+} Item;
+
+
+// struct for the depot
+typedef struct {
+    char* name;
+    Item* items;
+    int totalItems;
+} Depot;
+
+Status show_message(Status s) {
+    const char *messages[] = {"",
+            "Usage: 2310depot name {goods qty}\n",
+            "Invalid name(s)\n",
+            "Invalid quantity\n"};
+    fputs(messages[s], stderr);
+    return s;
+}
+
+int check_int(char* string) {
+    for (int i = 0; i < strlen(string); i++) {
+        // check integer
+        if (string[i] == '.' || string[i] == '-') {
+            return show_message(QUANERR);
+        }
+        // check that dimensions supplied are digits.
+        if (!isdigit(string[i])) {
+            return show_message(QUANERR);
+        }
+    }
+    
+    // check greater than 0
+    int numb = atoi(string);
+    if (numb < 0) {
+        return show_message(QUANERR);
+    }
+
+    return OK;
+}
+
+int parse(int argc, char** argv, Depot *info) {
+    /* 2310depot garden sand 10 seeds 2 */
+    // empty name
+    if (strlen(argv[1]) == 0) {
+        return show_message(NAMEERR);
+    }
+
+    // illegal char
+    for (int i = 0; i < strlen(argv[1]); i++) {
+        if ((argv[1][i] == ' ') || (argv[1][i] == '\n') || (argv[1][i] == '\r') 
+            || (argv[1][i] == ':')) {
+            return show_message(NAMEERR);
+        }
+    }
+    
+    // set name
+    info->name = argv[1];
+
+    // parse items
+    info->items = malloc(sizeof(Item) * ((argc - 2) / 2));
+    int pos = 0;
+    for (int i = 2; i < argc; i++) {
+        if (i % 2 == 0) {
+            // parse item name
+            Item item;
+            item.name = argv[i];
+            info->items[pos] = item;
+        } else {
+            // parse item quantity.
+            int countStatus = check_int(argv[i]);
+            if (countStatus != 0) {
+                return countStatus;
+            }
+            info->items[pos].count = atoi(argv[i]);
+            pos++;
+        }
+    }
+    info->totalItems = pos;
+
+    return OK;
+}
+
+int start_up(int argc, char** argv) {
+    Depot info;
+
+    // parse args
+    int parseStatus = parse(argc, argv, &info);
+    if (parseStatus != 0) {
+        return parseStatus;
+    }
+
+    //todo DEBUG REMOVE
+    for (int i = 0; i < info.totalItems; i++) {
+        printf("%s:%d\n", info.items[i].name, info.items[i].count);
+    }
+
+    // listen 
+
+    return OK;
+
+}
+
 
 int main(int argc, char** argv) {
-    printf("hi");
+    if ((argc % 2) != 0 || argc < 2) {
+        show_message(INCORRARGS);
+    } else {
+        return start_up(argc, argv);
+    }
 }
